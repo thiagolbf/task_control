2554c47f91feea2ddc2c312914462246fda6c60d
Thiago Barcelos <thiagolbf@hotmail.com>
2023-06-14 20:49:11 -0300

    feat: styled theme button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { HeaderContainer, Title } from "./style";
+import { HeaderContainer, Title, ToggleTheme } from "./style";
 
 // import Switch from "react-switch";
 import { useContext } from "react";
@@ -6,11 +6,14 @@ import { ThemeContext } from "styled-components";
 
 import Logo from "../../assets/rocket.svg";
 
+import Switch from "react-switch";
+
 interface HeaderProps {
   changeTheme: () => void;
+  actualTheme: string;
 }
 
-export const Header = ({ changeTheme }: HeaderProps) => {
+export const Header = ({ changeTheme, actualTheme }: HeaderProps) => {
   const theme = useContext(ThemeContext);
 
   return (
@@ -22,7 +25,20 @@ export const Header = ({ changeTheme }: HeaderProps) => {
           <span>control</span>
         </Title>
 
-        <button onClick={() => changeTheme()}>Trocar tema</button>
+        {/* <button onClick={() => changeTheme()}>Trocar tema</button> */}
+        <ToggleTheme>
+          <Switch
+            onChange={() => changeTheme()}
+            checked={actualTheme === "dark"}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={10}
+            width={40}
+            handleDiameter={20}
+            onColor={theme.blueDark}
+            className="toggleTheme"
+          />
+        </ToggleTheme>
       </HeaderContainer>
     </>
   );
